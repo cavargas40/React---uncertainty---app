1e9bbe2d5edc5fcccc9a586d5243ee906137b6dc
Carlos Vargas <cavargas40@gmail.com>
2021-04-11 20:24:41 -0500

    setting up npm and babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,13 @@
+"use strict";
+
 console.log("app.js is running");
 
 // JSX - JavaScript XML
-// const template = <p>This is JSX form app.js!</p>;
-const template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "Something New"
-)
-const appRoot = document.getElementById("app");
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App!"
+);
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
